31df2e22fafc9cf8f9221799bc1d818dbda5606b
nunuNNN <18920275639@163.com>
2022-04-08 19:56:04 +0800

    第二次提交

--- src/sins/SINS.cpp
+++ src/sins/SINS.cpp
@@ -31,17 +31,15 @@ void SINS::Init(const Quat &qnb0, const Vect3 &vn0, const Vect3 &pos0, double tk
 	pos = pos0;
 	Cbn = I33;
 
-	// 
 	Kg = Ka = I33; 
 	eb = db = Ka2 = O31;
 
 	// 递推过程中协方差
 	Maa = Mav = Map = Mva = Mvv = Mvp = Mpv = Mpp = O33;
 
-	lvr = O31;
+	lvr = O31; an = anbar = webbar = O31;
 
 	Vect3 wib(0.0), fb=(~qnb)*Vect3(0,0,glv.g0);
-	an = anbar = webbar = O31;
 	isOpenloop = 0;
 
 	Update(&wib, &fb, 1, 1.0); imu.preFirst = 1;
@@ -49,12 +47,61 @@ void SINS::Init(const Quat &qnb0, const Vect3 &vn0, const Vect3 &pos0, double tk
 	tk = tk0;  ts = nts = 1.0; 
 	qnb = qnb0;	vn = vn0, pos = pos0;
 	
-	mmwb = mmfb = CMaxMin(100);
-	mvn = mvnmax = mvnmin = vn; mvni = O31; mvnt = mvnT = 0.0; mvnk = 0;
-	etm(); lever(); Extrap();
+	etm(); lever();
 }
 
+void SINS::Update(const CVect3 *pwm, const CVect3 *pvm, int nSamples, double ts)
+{
+	this->ts = ts;  nts = nSamples*ts;	tk += nts;
+	double nts2 = nts/2;
+	imu.Update(pwm, pvm, nSamples);
+	imu.phim = Kg*imu.phim - eb*nts; imu.dvbm = Ka*imu.dvbm - db*nts;  // IMU calibration
+	imu.dvbm.i -= Ka2.i*imu.dvbm.i*imu.dvbm.i/nts;
+	imu.dvbm.j -= Ka2.j*imu.dvbm.j*imu.dvbm.j/nts;
+	imu.dvbm.k -= Ka2.k*imu.dvbm.k*imu.dvbm.k/nts;
+//		CVect3 vn01 = vn+an*nts2, pos01 = pos+eth.vn2dpos(vn01,nts2);
+	if(!isOpenloop) eth.Update(pos,O31,1);
+	wib = imu.phim/nts; fb = imu.dvbm/nts;
+	web = wib;  webbar = (1-afabar)*webbar + afabar*web;
+	wnb = wib;
+	fn = qnb*fb;
+	an = fn+eth.gcc;  anbar = (1-afabar)*anbar + afabar*an;
+	CVect3 vn1 = vn + an*nts;
+	pos = pos + eth.vn2dpos(vn+vn1, nts2);	vn = vn1;
+	qnb = qnb*rv2q(imu.phim);
+	Cnb = q2mat(qnb); att = m2att(Cnb); Cbn = ~Cnb; vb = Cbn*vn;
 
 
+	if(vn.i>velMax) vn.i=velMax; else if(vn.i<-velMax) vn.i=-velMax;
+	if(vn.j>velMax) vn.j=velMax; else if(vn.j<-velMax) vn.j=-velMax;
+	if(vn.k>velMax) vn.k=velMax; else if(vn.k<-velMax) vn.k=-velMax;
+
+	if(pos.i>89.9*DEG) pos.i=89.9*DEG; else if(pos.i<-89.9*DEG) pos.i=-89.9*DEG;
+	if(pos.j>PI) pos.j-=_2PI; else if(pos.j<-PI) pos.j+=_2PI;
+	if(pos.k>hgtMax) pos.k=hgtMax; else if(pos.k<hgtMin) pos.k=hgtMin;
+
+}
+
+void SINS::lever(const CVect3 &dL, CVect3 *ppos, CVect3 *pvn)
+{
+	if(&dL!=&O31) lvr = dL;
+//	Mpv = CMat3(0,eth.f_RMh,0, eth.f_clRNh,0,0, 0,0,1);
+	Mpv.e01=eth.f_RMh, Mpv.e10=eth.f_clRNh, Mpv.e22=1.0;
+	CW = Cnb*askew(web), MpvCnb = Mpv*Cnb;
+	if(ppos==NULL) {
+		posL = pos + MpvCnb*lvr;
+		if(pvn==NULL)  vnL = vn + CW*lvr;
+	} 
+	else {
+		*ppos = pos + MpvCnb*lvr;
+		if(pvn!=NULL)  *pvn = vn + CW*lvr;
+	}
+}
+
+void SINS::etm(void)
+{
+	Mva = askew(fn);
+	Mpv = CMat3(0,eth.f_RMh,0, eth.f_clRNh,0,0, 0,0,1);
+}
 
 
--- src/sins/SINS.h
+++ src/sins/SINS.h
@@ -20,10 +20,28 @@ class SINS
 {
 public:
     double ts, nts, tk;
-    
+	double mvnt, mvnT, velMax, hgtMin, hgtMax, afabar;
+	int mvnk;
+	CEarth eth;
+	CIMU imu;
+	CQuat qnb;
+	CMat3 Cnb, Cnb0, Cbn, mvnCnb0, Kg, Ka;
+	CVect3 wib, fb, fn, an, anbar, web, webbar, wnb, att, vn, mvn, mvni, mvnmax, mvnmin, vb, pos, eb, db, Ka2, tauGyro, tauAcc, _betaGyro, _betaAcc;
+	CMat3 Maa, Mav, Map, Mva, Mvv, Mvp, Mpv, Mpp;	// for etm
+	CVect3 lvr, vnL, posL; 
+    CMat3 CW, MpvCnb;		// for lever arm
+	BOOL isOpenloop;
+
+public:
+	CEarth eth;
+	CIMU imu;
+
 public:
     SINS(const Vect3 &att0, const Vect3 &vn0=O31, const Vect3 &pos0=O31, double tk0=0.0);
     void Init(const Quat &qnb0=qI, const Vect3 &vn0=O31, const Vect3 &pos0=O31, double tk0=0.0);    // initialization using quat attitude, velocity & position
+	void Update(const CVect3 *pwm, const CVect3 *pvm, int nSamples, double ts);		// SINS update using Gyro&Acc samples
+    void lever(const CVect3 &dL=O31, CVect3 *ppos=NULL, CVect3* pvn=NULL);		// lever arm
+    void etm(void);							// SINS error transform matrix coefficients
 
 };
 
